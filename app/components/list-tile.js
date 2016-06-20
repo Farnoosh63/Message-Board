969ef914a1879673f0d8a61fa1a1819e8d14257f
Farnoosh <farnoosh@pdx.edu>
2016-06-19 23:19:23 -0700

    fix the list-tile edit and delete button

--- app/components/list-tile.js
+++ app/components/list-tile.js
@@ -9,6 +9,23 @@ export default Ember.Component.extend({
   actions: {
     addToFav(list) {
       this.get('favorite').add(list);
+
+  },
+  delete(list){
+    if (confirm('Are you sure you want to delete this question?')) {
+      list.destroyRecord();
+      // this.transitionTo('index');
     }
+  },
+  editList(list, params) {
+    Object.keys(params).forEach(function(key) {
+      if(params[key]!==undefined) {
+        list.set(key, params[key]);
+      }
+    });
+    list.save();
+    this.transitionTo('index');
   }
+}
+
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -8,33 +8,33 @@ export default Ember.Route.extend({
     });
   },
   actions: {
-    delete(list){
-      if (confirm('Are you sure you want to delete this question?')) {
-        var list_deletions = list.get('answers').map(function(answer) {
-          return answer.destroyRecord();
-        });
-        Ember.RSVP.all(list_deletions).then(function() {
-          return list.destroyRecord();
-        });
-        this.transitionTo('index');
-      }
-
-    },
+    // delete(list){
+    //   if (confirm('Are you sure you want to delete this question?')) {
+    //     var list_deletions = list.get('answers').map(function(answer) {
+    //       return answer.destroyRecord();
+    //     });
+    //     Ember.RSVP.all(list_deletions).then(function() {
+    //       return list.destroyRecord();
+    //     });
+    //     this.transitionTo('index');
+    //   }
+    //
+    // },
     save(params) {
       var newQuestion = this.store.createRecord('list', params);
       newQuestion.save();
       this.transitionTo('index');
     },
 
-    editList(list, params) {
-      Object.keys(params).forEach(function(key) {
-        if(params[key]!==undefined) {
-          list.set(key, params[key]);
-        }
-      });
-      list.save();
-      this.transitionTo('index');
-    },
+    // editList(list, params) {
+    //   Object.keys(params).forEach(function(key) {
+    //     if(params[key]!==undefined) {
+    //       list.set(key, params[key]);
+    //     }
+    //   });
+    //   list.save();
+    //   this.transitionTo('index');
+    // },
     saveContact(contactParams){
       var newContact = this.store.createRecord('contact', contactParams);
       newContact.save();
